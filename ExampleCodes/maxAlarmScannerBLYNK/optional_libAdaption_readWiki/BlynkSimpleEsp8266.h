a15be4b32fcf37a11255226224da83dd7bac3a92
Kina <37293282+iCounterBOX@users.noreply.github.com>
2018-09-03 00:26:14 +0200

    Create BlynkSimpleEsp8266.h
    check our wiki on this please: https://github.com/iCounterBOX/dthack18/wiki/dthack18---maxAlarmScannerBLYNK
    
    its optional!! Its just to avoid EndlessLoop in case wifi connection issues!!
    
    Take care if BLYNK is updating its LIB´s !!!!

---
+++ ExampleCodes/maxAlarmScannerBLYNK/optional_libAdaption_readWiki/BlynkSimpleEsp8266.h
@@ -0,0 +1,111 @@
+/**
+ * @file       BlynkSimpleEsp8266.h
+ * @author     Volodymyr Shymanskyy
+ * @license    This project is released under the MIT License (MIT)
+ * @copyright  Copyright (c) 2015 Volodymyr Shymanskyy
+ * @date       Jan 2015
+ * @brief
+ *
+ */
+
+#ifndef BlynkSimpleEsp8266_h
+#define BlynkSimpleEsp8266_h
+
+#ifndef ESP8266
+#error This code is intended to run on the ESP8266 platform! Please check your Tools->Board setting.
+#endif
+
+#include <BlynkApiArduino.h>
+#include <Blynk/BlynkProtocol.h>
+#include <Adapters/BlynkArduinoClient.h>
+#include <ESP8266WiFi.h>
+
+class BlynkWifi
+    : public BlynkProtocol<BlynkArduinoClient>
+{
+    typedef BlynkProtocol<BlynkArduinoClient> Base;
+public:
+    BlynkWifi(BlynkArduinoClient& transp)
+        : Base(transp)
+    {}
+
+    void connectWiFi(const char* ssid, const char* pass)
+    {
+        BLYNK_LOG2(BLYNK_F("Connecting to "), ssid);
+        WiFi.mode(WIFI_STA);
+        if (WiFi.status() != WL_CONNECTED) {
+            if (pass && strlen(pass)) {
+                WiFi.begin(ssid, pass);
+            } else {
+                WiFi.begin(ssid);
+            }
+        }
+		
+        //Mrs. Christin Koss / CK / ITcon :26.08.18 - No endles loop..mcu might hang we leave the loop
+		int wifiCounter = 0;
+		while (WiFi.status() != WL_CONNECTED) {
+			BlynkDelay(500);
+			Serial.print("BLYNK loop wifi waiting  for connection/ new Wifi-State: ");
+			Serial.println(WiFi.status());
+			//12.12.17/ nach 100 Versuchen Resetten wir den chip
+			if (++wifiCounter >= 100) {
+				Serial.println("WIFI_Connect()/ NO WIFI  !! RESET");
+				wifiCounter = 0;
+				wdt_reset(); ESP.restart(); while (1)wdt_reset();		//ESP.restart();
+				break;
+			}
+		}
+		
+        BLYNK_LOG1(BLYNK_F("Connected to WiFi"));
+
+        IPAddress myip = WiFi.localIP();
+        BLYNK_LOG_IP("IP: ", myip);
+    }
+
+    void config(const char* auth,
+                const char* domain = BLYNK_DEFAULT_DOMAIN,
+                uint16_t    port   = BLYNK_DEFAULT_PORT)
+    {
+        Base::begin(auth);
+        this->conn.begin(domain, port);
+    }
+
+    void config(const char* auth,
+                IPAddress   ip,
+                uint16_t    port = BLYNK_DEFAULT_PORT)
+    {
+        Base::begin(auth);
+        this->conn.begin(ip, port);
+    }
+
+    void begin(const char* auth,
+               const char* ssid,
+               const char* pass,
+               const char* domain = BLYNK_DEFAULT_DOMAIN,
+               uint16_t    port   = BLYNK_DEFAULT_PORT)
+    {
+        connectWiFi(ssid, pass);
+        config(auth, domain, port);
+        while(this->connect() != true) {}
+    }
+
+    void begin(const char* auth,
+               const char* ssid,
+               const char* pass,
+               IPAddress   ip,
+               uint16_t    port   = BLYNK_DEFAULT_PORT)
+    {
+        connectWiFi(ssid, pass);
+        config(auth, ip, port);
+        while(this->connect() != true) {}
+    }
+
+};
+
+static WiFiClient _blynkWifiClient;
+static BlynkArduinoClient _blynkTransport(_blynkWifiClient);
+BlynkWifi Blynk(_blynkTransport);
+
+#include <BlynkWidgets.h>
+
+#endif
